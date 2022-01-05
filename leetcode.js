00ca9fcb83be9943f8bde5b723f26cddc186c407
komiljon4717 <komiljon4717@gmail.com>
2022-01-05 20:52:23 +0500

    412-problem done

--- leetcode.js
+++ leetcode.js
@@ -511,10 +511,29 @@
 // console.log(singleNumber([2, 2, 1]));
 
 
+/*=============================412==============================*/
+var fizzBuzz = function(n) {
+    let res = []
+    for (let i = 1; i <= n; i++) {
+        if (i % 3 ===0 && i % 5 === 0) {
+            res.push('FizzBuzz')
+            continue
+        }
+        if (i % 3 === 0) {
+            res.push('Fizz')
+            continue
+        }
+        if (i % 5 === 0) {
+            res.push('Buzz')
+            continue
+        }
+        else res.push(`${i}`)
+        
+    }
+    return res
+};
 
-
-
-
+console.log(fizzBuzz(15));
 
 
 
