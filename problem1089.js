6f82563dd1a302fb2cfdd29adebcec9c52b91be6
komiljon4717 <komiljon4717@gmail.com>
2022-08-29 19:33:50 +0500

    ishlandi

---
+++ problem1089.js
@@ -0,0 +1,13 @@
+function duplicateZeros(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 0) {
+            arr.splice(i + 1, 0, 0)
+            arr.pop()
+            i++
+        }
+    }
+    return arr
+}
+
+
+console.log(duplicateZeros([1,0,2,3,0,4,5,0]));
